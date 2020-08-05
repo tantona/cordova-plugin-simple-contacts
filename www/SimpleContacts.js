cdb1f4c3dc2729f1b22746bffad7ba516d650442
Alexander Tanton <tantona@gmail.com>
2020-08-05 13:08:20 -0500

    use a promise for listing contacts

--- www/SimpleContacts.js
+++ www/SimpleContacts.js
@@ -4,8 +4,8 @@ const PLUGIN_NAME = "SimpleContacts";
 const SimpleContacts = function () {};
 // All of your plugin functions go below this.
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-SimpleContacts.list = function (success, failure) {
-  exec(success, failure, PLUGIN_NAME, "list", []);
-};
+SimpleContacts.list = new Promise((resolve, reject) => {
+  exec(resolve, reject, PLUGIN_NAME, "list", []);
+});
 
 module.exports = SimpleContacts;
